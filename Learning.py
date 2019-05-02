8f5825574f0ad7030cca3b12f2220671e91d766a
Rivka Schuss <rschuss123@gmail.com>
2019-05-02 14:35:01 +0300

    clusters

--- Learning.py
+++ Learning.py
@@ -12,7 +12,7 @@ start_date = datetime.date(2017, 6, 13)
 def learn(h_list):
     hyrax_dict, basestation_dict, start, end = pd.parse_details()
 
-    days_cluster_list, night_cluster_list = get_cluster_per_day(h_list, hyrax_dict, min_meeting_length=20)
+    days_cluster_list, night_cluster_list = get_cluster_per_day(h_list, min_meeting_length=20)
 
 
     save_count_per_day_to_file(h_list, hyrax_dict)
@@ -58,26 +58,16 @@ def save_count_per_day_to_file(h_list, hyrax_dict):
                         days_count[delta.days] += 1
 
 
-def get_cluster_per_day(h_list, hyrax_dict, min_meeting_length):
+def get_cluster_per_day(h_list, min_meeting_length):
     days = dict()
     nights = dict()
-    day_clusters = dict()
-    night_clusters = dict()
     date_times = h.initialize_specific_range()
     for date in date_times:
         days[date] = list()
         nights[date] = list()
-        day_clusters[date] = dict()
-        night_clusters[date] = dict()
-
     for i in h_list:
-        hy_dict = {}
-        # hy_dict["hyrax"] = i
-        # hy_dict['Sex'] = hyrax_dict[i].sex
         for j in h_list:
             if i != j:
-                # days_count = [0] * 61
-                # night_count = [0] * 61
                 mtx_a = h.load_lil(h.get_npz_file_name(i, j))
                 non_zero = mtx_a.nonzero()[1]
                 current_date = None
@@ -87,42 +77,58 @@ def get_cluster_per_day(h_list, hyrax_dict, min_meeting_length):
                     if meeting_counter == 0:
                         current_date = date
                     if current_date is not date:
-                        days[current_date].append(Meeting(i, j, meeting_counter))
+                        if night == 1:
+                            nights[current_date].append(Meeting(i, j, meeting_counter))
+                        else:
+                            days[current_date].append(Meeting(i, j, meeting_counter))
                         meeting_counter = 0
                         continue
                     else:
                         meeting_counter += 1
-                        #if night == 1:
-                            #nights[date].append([i, j, time])
-                        #else
+
+    day_clusters = create_clusters(days, min_meeting_length, date_times)
+    night_clusters = create_clusters(nights, min_meeting_length, date_times)
+
+    return day_clusters, night_clusters
 
 
-    cluster_count_day = 0
-    for date, meetings in days.items():
+def create_clusters(dic_dates, min_meeting_length, date_times):
+    clusters = dict()
+    for date in date_times:
+        clusters[date] = dict()
+    for date, meetings in dic_dates.items():
+        cluster_count_day = 1
         for meeting in meetings:
             length = meeting.length
             if length >= min_meeting_length:
-                if cluster_count_day == 0:
-                    day_clusters[date][0] = meeting.i, meeting.j
+                if len(clusters[date]) == 0:
+                    clusters[date][meeting.j] = cluster_count_day
+                    clusters[date][meeting.i] = cluster_count_day
                 else:
-                    cluster_count_day += 1
-                    day_clusters[date][cluster_count_day] = meeting.i, meeting.j
-                    #for comp_meeting in meetings:
-                        #if meeting is not comp_meeting:
+                    i_cluster_val = clusters[date].get(meeting.i)
+                    j_cluster_val = clusters[date].get(meeting.j)
+                    if i_cluster_val is None and j_cluster_val is None:
+                        cluster_count_day += 1
+                        clusters[date][meeting.j] = cluster_count_day
+                        clusters[date][meeting.i] = cluster_count_day
+                    elif i_cluster_val is not None and j_cluster_val is None:
+                        cluster_count_day += 1
+                        clusters[date][meeting.j] = cluster_count_day
+                        clusters[date][meeting.i] = i_cluster_val
+                    elif j_cluster_val is not None and i_cluster_val is None:
+                        cluster_count_day += 1
+                        clusters[date][meeting.i] = cluster_count_day
+                        clusters[date][meeting.j] = j_cluster_val
+                    else:
+                        clusters[date][meeting.i] = i_cluster_val
+                        clusters[date][meeting.j] = j_cluster_val
+    return clusters
 
-            #for comp in meetings:
-    #for date in day_clusters:
-
-
-
-
-
-
-    return day_clusters, night_clusters
 
 def save_list(list_name, list_var):
     with open(list_name, "w") as file:
         file.write(str(list_var))
+
 
 class Meeting:
     def __init__(self, i, j, length):
--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import Learning as lr
 def main():
 
     personal_list = [2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 19, 21, 22, 23, 24, 25, 26, 29, 33, 36, 39]
-    calculated_list = [4, 11, 24, 33, 39]
-    #calculated_list = [2, 7]
+    # calculated_list = [4, 11, 24, 33, 39]
+    calculated_list = [4, 11, 24]
 
     lr.learn(calculated_list)
     # with open('dataSet.csv', 'a') as outcsv:
